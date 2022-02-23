968b1b9ef2b28d0000808efc3c97fccf6e0c8a6f
Joshua Morony <joshua.morony@gmail.com>
2022-02-23 14:45:02 +1030

    wip: #21 added unit test

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -2,23 +2,42 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
+import { AuthService } from '../shared/data-access/auth.service';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HomePage ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  let mockAuthService: Pick<AuthService, 'loginWithGoogle'>;
 
-    fixture = TestBed.createComponent(HomePage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      mockAuthService = {
+        loginWithGoogle: jest.fn(),
+      };
+
+      TestBed.configureTestingModule({
+        declarations: [HomePage],
+        imports: [IonicModule.forRoot()],
+        providers: [{ provider: AuthService, useValue: mockAuthService }],
+      }).compileComponents();
+
+      fixture = TestBed.createComponent(HomePage);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should trigger authentication process with google', () => {
+    const authService =
+      fixture.debugElement.injector.get<AuthService>(AuthService);
+
+    component.login();
+
+    expect(authService.loginWithGoogle).toHaveBeenCalled();
   });
 });
