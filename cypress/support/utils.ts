29057c93574c30f61f566e32c64567408d35f7dc
Joshua Morony <joshua.morony@gmail.com>
2022-03-03 21:55:59 +1030

    wip: #2 added e2e test

--- cypress/support/utils.ts
+++ cypress/support/utils.ts
@@ -1,2 +1,3 @@
 export const getLoginButton = () => cy.get('[data-test=login-button]');
 export const getTitle = () => cy.get('[data-test=page-title]');
+export const getItemsInList = () => cy.get('[data-test=list] ion-item');
