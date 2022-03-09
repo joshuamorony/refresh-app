d507cc6b492a4bc0d8bb73c02a1e3804e3581c0d
Joshua Morony <joshua.morony@gmail.com>
2022-03-09 23:00:45 +1030

    fix: ci

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -49,12 +49,23 @@ import 'firebase/compat/firestore';
 import { attachCustomCommands } from 'cypress-firebase';
 import { environment } from '../../src/environments/environment';
 
-firebase.initializeApp(environment.firebase);
+firebase.initializeApp({
+  projectId: 'refresh-module',
+  appId: '1:746470688865:web:88951d2ab0275b0575c220',
+  storageBucket: 'refresh-module.appspot.com',
+  locationId: 'us-central',
+  apiKey: 'AIzaSyAaxXgeaupPhuK3jMjqtaaQzetd-y6cIN8',
+  authDomain: 'refresh-module.firebaseapp.com',
+  messagingSenderId: '746470688865',
+});
 
 const firestoreEmulatorHost = Cypress.env('FIRESTORE_EMULATOR_HOST');
+console.log('!!!!!!!!!!!!!!!!!!!!!!!');
+console.log(firestoreEmulatorHost);
 if (firestoreEmulatorHost) {
   firebase.firestore().settings({
     host: firestoreEmulatorHost,
+    experimentalForceLongPolling: true,
     ssl: false,
   });
 }
