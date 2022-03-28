187e8e427f08bc39e830474178a075de374c79de
Joshua Morony <joshua.morony@gmail.com>
2022-03-29 09:04:28 +1030

    wip: #14 added e2e test

---
+++ cypress/integration/survey.spec.ts
@@ -0,0 +1,28 @@
+import { getForm, getSubmitButton, getThankYouMessage } from '../support/utils';
+
+describe('Feedback', () => {
+  beforeEach(() => {
+    cy.callFirestore('delete', 'clients');
+
+    cy.intercept('GET', '/assets/survey-form.json', {
+      fixture: 'survey-form.json',
+    });
+
+    cy.callFirestore('set', `clients/abc123`, {
+      name: {
+        first: 'Josh',
+        last: 'Morony',
+      },
+    });
+
+    cy.visit('/client-history/abc123');
+  });
+
+  it('can submit survey form', () => {
+    getForm().should('exist');
+    getSubmitButton().click();
+    cy.get('form', { timeout: 10000 }).should('not.exist');
+    getSubmitButton().should('not.exist');
+    getThankYouMessage().should('be.visible');
+  });
+});
